b710cfd03b07069de671b264c09ac32e0a42715d
Vadim <146763203+expens1veee@users.noreply.github.com>
2024-10-18 03:19:22 +0300

    Delete PycharmProjects/pythonProject directory

--- PycharmProjects/pythonProject/HB_HSCMS_BOT/global_variables/states.py
+++
@@ -1,6 +0,0 @@
-from vkbottle import BaseStateGroup
-
-
-class BotStates(BaseStateGroup):
-    CHECK_ANSWER = 1
-    INPUT_SPAM = 2
--- PycharmProjects/pythonProject/HB_HSCMS_BOT/start.py
+++
@@ -1,4 +0,0 @@
-from main import start_bot
-
-if __name__ == "__main__":
-    start_bot()
